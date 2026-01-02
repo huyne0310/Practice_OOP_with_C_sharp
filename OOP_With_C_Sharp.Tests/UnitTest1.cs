398a8df362ee9abc65a3d17ae68325920ff99672
HuyNV310 <nguyenhuy03102007@gmail.com>
2026-01-03 00:11:08 +0700

    Add project files.

---
+++ OOP_With_C_Sharp.Tests/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using BankAccountSystem;
+using Xunit;
+
+public class BankAccountTests
+{
+    [Theory]
+    [InlineData(50)]
+    [InlineData(100)]
+    [InlineData(100.8)]
+    public void Deposit_ValidAmount_IncreasesBalance(decimal amount)
+    {
+        var acc = new BankAccount("123", "Test1");
+
+        acc.Deposit(amount);
+
+        Assert.Equal(amount, acc.Balance);
+    }
+    [Fact]
+    public void Deposit_InValidAmount_ThrowEx()
+    {
+        var acc = new BankAccount("ab2", "test2");
+        
+        Assert.Throws<InvalidOperationException>(() => acc.Deposit(-50));
+    }
+
+    [Fact]
+    public void Withdraw_Valid_DecreaseBalance()
+    {
+        var acc = new BankAccount("abc", "test3");
+        acc.Deposit(200);
+        acc.Withdraw(100);
+        Assert.Equal(100, acc.Balance);
+    }
+
+    [Fact]
+    public void Withdraw_Invalid_ThrowEx()
+    {
+        var acc = new BankAccount("dasda", "test4");
+        //acc.Withdraw(50);
+        Assert.Throws<InvalidOperationException>(() => acc.Withdraw(50));
+    }
+
+}
+
+public class FakeITransactionLogger : ITransactionLogger
+{
+    public bool WasCalled { get; private set; }
+    public string mess { get; private set;  }
+
+    public void Log(string message)
+    {
+        WasCalled = true;
+        mess = message;
+    }
+
+}
+
+public class BankService_Test
+{
+
+
+    [Theory]
+    [InlineData(1000,10,200)]
+    [InlineData(900, 10, 300)]
+    [InlineData(1200, 54.3, 200)]
+    [InlineData(1300.88, 0.55, 197.36)]
+    [InlineData(1818, 36.36, 18.36)]
+    public void Transfer_Valid_Sendlog(decimal bal1, decimal bal2, decimal amount)
+    {
+        var log = new FakeITransactionLogger();
+        var service = new BankService(log);
+        var acc1 = new BankAccount("A", "nguoi gui");
+        var acc2 = new BankAccount("B", "nguoi nhan");
+        acc1.Deposit(bal1);
+        acc2.Deposit(bal2);
+        service.Transfer(acc1, acc2, amount);
+        Assert.Equal((bal1 - amount), acc1.Balance);
+        Assert.Equal((bal2 + amount), acc2.Balance);
+
+        Assert.True(log.WasCalled);
+        Assert.Equal($"Transfer {amount} from {acc1.AccountNumber} to {acc2.AccountNumber}", log.mess);
+    }
+    [Theory]
+    [InlineData(1000, 10, 2000)]
+    [InlineData(900, 10, 3000)]
+    public void Transfer_InValidAmount_ThrowEx(decimal bal1, decimal bal2, decimal amount)
+    {
+        var log = new FakeITransactionLogger();
+        var service = new BankService(log);
+        var acc1 = new BankAccount("A", "nguoi gui");
+        var acc2 = new BankAccount("B", "nguoi nhan");
+        acc1.Deposit(bal1);
+        acc2.Deposit(bal2);
+        
+        Assert.Throws<InvalidOperationException>(() => service.Transfer(acc1, acc2, amount));
+        Assert.False(log.WasCalled);
+        Assert.Throws<ArgumentNullException>(() => new BankService(null));
+    }
+}
---
+++ OOP_With_C_Sharp/ENCAPSULATION_VALIDATION.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace BankAccountSystem
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; private set; }
+        public string OwnerName { get; }
+        public decimal Balance { get; private set; }
+
+        public BankAccount(string accountNumber, string ownerName)
+        {
+            Balance = 0;
+            //if (accountNumber == "" || ownerName == "" || accountNumber == null || ownerName == null)
+            if (string.IsNullOrWhiteSpace(accountNumber) || string.IsNullOrWhiteSpace(ownerName))
+            {
+                throw new ArgumentException();
+            }
+            AccountNumber = accountNumber;
+            OwnerName = ownerName;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0 || amount > Balance)
+            {
+                throw new InvalidOperationException();
+            }
+            Balance -= amount;
+        }
+    }
+
+    public interface ITransactionLogger
+    {
+        void Log(string message);
+    }
+
+
+    public class BankService {
+
+        private readonly ITransactionLogger _logger;
+        public BankService(ITransactionLogger logger)
+        {
+            //if (logger == null)
+            //{
+            //    throw new ArgumentNullException();
+            //}
+            //_logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+
+        }
+
+        public void Transfer(BankAccount from, BankAccount to, decimal amount)
+        {
+            if (from == null || to == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (amount <= 0 )
+            {
+                throw new InvalidOperationException();
+            }
+            from.Withdraw(amount);
+            to.Deposit(amount);
+
+            _logger.Log($"Transfer {amount} from {from.AccountNumber} to {to.AccountNumber}");
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
