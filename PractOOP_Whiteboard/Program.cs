1036a4ce87e46cb2c91f67ee91c64c81bad63edf
HuyNV310 <nguyenhuy03102007@gmail.com>
2026-01-11 15:59:41 +0700

    Creating a simple logic whiteborad app XUnit tests

---
+++ PractOOP_Whiteboard/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Whiteboard
+{
+    public class Stroke
+    {
+        public int Id {  get; private set; }
+        public string Color { get; private set; }
+        public int Thickness { get; private set; }
+        public string Content { get; private set; }
+
+        public Stroke(int id, string color, int thickness, string content)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException(nameof(id));
+            }
+
+            else if (String.IsNullOrEmpty(color))
+            {
+                throw new ArgumentException(nameof(color));
+            }
+            else if (thickness <= 0)
+            {
+                throw new ArgumentException(nameof(thickness));
+            }
+            else if (String.IsNullOrEmpty(content)) { 
+                throw new ArgumentException(nameof(content));
+            }
+
+            Id = id;
+            Color = color;
+            Thickness = thickness;
+            Content = content;
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void Log(string message) { 
+            Console.WriteLine( "[Log] " + message);
+        }
+    }
+
+    public class WhiteBoard
+    {
+        private List<Stroke> strokes = new List<Stroke>();
+        private Stack<Stroke> undoStack = new Stack<Stroke>();
+        private Stack<Stroke> redoStack = new Stack<Stroke>();
+        private readonly ILogger _log;
+
+        public WhiteBoard(ILogger log)
+        {
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+        }
+
+        public void AddStroke(Stroke tmp)
+        {
+            
+            strokes.Add(tmp);
+            undoStack.Push(tmp);
+            redoStack.Clear();
+            _log.Log("Added stroke " + tmp.Id);
+        }
+
+        public void Undo()
+        {
+            if (undoStack.Count() == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            redoStack.Push(undoStack.Peek());
+            strokes.Remove(undoStack.Peek());
+            
+            _log.Log("Undo add " + undoStack.Peek().Id);
+            undoStack.Pop();
+        }
+
+        public void Redo()
+        {
+            if (redoStack.Count() == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            
+            undoStack.Push (redoStack.Peek());
+            strokes.Add (redoStack.Peek());
+
+            _log.Log("Redo add " + redoStack.Peek().Id);
+
+            redoStack.Pop();
+        }
+
+        public IReadOnlyList<Stroke> GetStrokes()
+        {
+            return new List<Stroke>(strokes);
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            var _Strokes = new Stroke(1, "blue", 3, "A to B");
+            var workspace = new WhiteBoard(logger);
+            workspace.AddStroke(_Strokes);
+            workspace.Undo();
+        }
+    }
+}
---
+++ Test_Whiteboard/UnitTest1.cs
@@ -0,0 +1,136 @@
+﻿using Xunit;
+using Whiteboard;
+
+namespace Test_Whiteboard
+{
+    public class Test_Stroke
+    {
+        [Fact]
+        public void AddStroke_validData_CreateStroke()
+        {
+            var _stroke = new Stroke(1, "red", 3, "A to B");
+            Assert.Equal(1, _stroke.Id);
+            Assert.Equal("red", _stroke.Color);
+            Assert.Equal(3, _stroke.Thickness);
+            Assert.Equal("A to B", _stroke.Content);
+        }
+
+        [Fact]
+        public void AddStroke_InvalidId_ThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() => new Stroke(-1, "blue", 4, "B to C"));
+        }
+        [Fact]
+        public void AddStroke_InvalidColr_ThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() => new Stroke(2, "", 5, "C to D"));
+        }
+        [Fact]
+        public void AddStroke_InvalidThck_ThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() => new Stroke(3, "red", 0, "C to D"));
+        }
+        [Fact]
+        public void AddStroke_InvalidCntn_ThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() => new Stroke(4, "yellow", 1, ""));
+        }
+        // không gán được giá trị null nên không test
+
+    }
+
+    public class FakeILogger : ILogger
+    {
+        public bool WasCalled { get; private set; }
+        public string mess {  get; private set; }
+
+        public void Log(string messenger)
+        {
+            mess = messenger;
+            WasCalled = true;
+        }
+
+    }
+
+    public class Test_WhiteBoard
+    {
+        [Theory]
+        [InlineData(1, "red", 3, "A to B")]
+        [InlineData(2, "blue", 5, "A to B")]
+        [InlineData(3, "red", 3, "C to D")]
+        [InlineData(4, "red", 3, "B to C")]
+        [InlineData(5, "red", 3, "D to E")]
+        public void AddStroke_Valid_Add(int id, string color, int thickness, string content)
+        {
+            var log = new FakeILogger();
+            var _stroke = new Stroke(id, color, thickness, content);
+            var whiteboard = new WhiteBoard(log);
+
+            whiteboard.AddStroke(_stroke);
+            Assert.Equal(1, whiteboard.GetStrokes().Count());
+        }
+
+        [Fact]
+        public void AddStrokeLog_Valid_CallLog()
+        {
+            var log = new FakeILogger();
+            var _stroke = new Stroke(1, "red", 3, "A to B");
+            var whiteboard = new WhiteBoard(log);
+
+            whiteboard.AddStroke(_stroke);
+
+            Assert.True(log.WasCalled);
+        }
+
+        [Theory]
+        [InlineData(1, "red", 3, "A to B")]
+        [InlineData(2, "blue", 5, "A to B")]
+        [InlineData(3, "red", 3, "C to D")]
+        [InlineData(4, "red", 3, "B to C")]
+        [InlineData(5, "red", 3, "D to E")]
+
+        public void Undo_Valid_undo(int id, string color, int thickness, string content)
+        {
+            var log = new FakeILogger();
+            var _stroke = new Stroke(id, color, thickness, content);
+            var whiteboard = new WhiteBoard(log);
+            whiteboard.AddStroke(_stroke);
+            whiteboard.Undo();
+            Assert.Equal(0, whiteboard.GetStrokes().Count());
+        }
+        [Fact]
+        public void Undo_Invalid_ThrowEx()
+        {
+            var log = new FakeILogger();
+            var whiteboard = new WhiteBoard(log);
+
+            Assert.Throws<InvalidOperationException>(() => whiteboard.Undo());
+        }
+
+        [Theory]
+        [InlineData(1, "red", 3, "A to B")]
+        [InlineData(2, "blue", 5, "A to B")]
+        [InlineData(3, "red", 3, "C to D")]
+        [InlineData(4, "red", 3, "B to C")]
+        [InlineData(5, "red", 3, "D to E")]
+        public void Redo_Valid_redo(int id, string color, int thickness, string content)
+        {
+            var log = new FakeILogger();
+            var _stroke = new Stroke(id, color, thickness, content);
+            var whiteboard = new WhiteBoard(log);
+            whiteboard.AddStroke(_stroke);
+            whiteboard.Undo();
+            whiteboard.Redo();
+            Assert.Equal(1, whiteboard.GetStrokes().Count());
+        }
+
+        [Fact]
+        public void Redo_Invalid_ThrowEx() {
+            var log = new FakeILogger();
+            var _stroke = new Stroke(1, "red", 3, "A to B");
+            var whiteboard = new WhiteBoard(log);
+            whiteboard.AddStroke(_stroke);
+            Assert.Throws<InvalidOperationException>(() => whiteboard.Redo());
+        }
+    }
+}
